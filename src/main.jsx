62fef99e1466c47e0610839e0496a97ae8f56e0d
parikshit2908 <parikshit.ranka2908@gmail.com>
2025-12-29 04:48:32 +0530

    fix: Android Chrome black screen by delaying GPU layers

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
+/*
+  Android Chrome render safety:
+  Delay heavy GPU layers until first paint
+*/
+requestAnimationFrame(() => {
+  document.documentElement.classList.add("app-ready");
+});
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
